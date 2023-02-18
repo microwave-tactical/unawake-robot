4212760e63984a80fdcdeeb9cb9e1bfcd8e7d0d8
Matteo Fazio <99208707+mcfazio2001@users.noreply.github.com>
2023-02-18 16:26:26 -0500

    Add files via upload

--- MakeUofT-2023-Robotic-Arm/MakeUofT-2023-Robotic-Arm.ino
+++ MakeUofT-2023-Robotic-Arm/MakeUofT-2023-Robotic-Arm.ino
@@ -6,17 +6,21 @@ Servo servo3; //arm extension
 
 //servo postions 
 
+//servo 2
+int s2pos1 =0; //start pos
+int s2pos2 =0; //end pos
+
+//servo 3
+int s3pos1 =0; //start pos
+int s3pos2 =0; //end pos
+
 //servo 4
 int s4pos1 =0; //start pos
 int s4pos2 =0; //end pos
 
+//robot on/off swtich
+int toggle = 5; 
 
-//servo3
-
-int toggle = 5; //robot on/off swtich
-
-int servo_position = 0;    //servo position
- 
 void setup() {
 
   //servo motors
@@ -30,6 +34,44 @@ void setup() {
   Serial.begin(9600);
 
 }
+
+//___________________________servo 2___________________________
+
+void servo2CW(int s4pos1, int s4pos2){ //servo4 clock wise rotation
+  for (int pos = s4pos1; pos <= s4pos2; pos += 1){
+    if (digitalRead(toggle) == LOW) break;
+    delay(8);
+    servo4.write(pos);
+  }
+}
+
+void servo2CCW(int s4pos1, int s4pos2){ //servo4 counter clock wise rotation
+  for (int pos = s4pos2; pos >= s4pos1; pos -= 1){
+    if (digitalRead(toggle) == LOW) break;
+    delay(8);
+    servo4.write(pos);
+  }
+}
+
+//___________________________servo 3___________________________
+
+void servo3CW(int s4pos1, int s4pos2){ //servo4 clock wise rotation
+  for (int pos = s4pos1; pos <= s4pos2; pos += 1){
+    if (digitalRead(toggle) == LOW) break;
+    delay(8);
+    servo4.write(pos);
+  }
+}
+
+void servo3CCW(int s4pos1, int s4pos2){ //servo4 counter clock wise rotation
+  for (int pos = s4pos2; pos >= s4pos1; pos -= 1){
+    if (digitalRead(toggle) == LOW) break;
+    delay(8);
+    servo4.write(pos);
+  }
+}
+
+//___________________________servo 4___________________________
 
 void servo4CW(int s4pos1, int s4pos2){ //servo4 clock wise rotation
   for (int pos = s4pos1; pos <= s4pos2; pos += 1){
@@ -46,6 +88,8 @@ void servo4CCW(int s4pos1, int s4pos2){ //servo4 counter clock wise rotation
     servo4.write(pos);
   }
 }
+
+
 
 
 

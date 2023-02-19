7db1ea847ef405caba237096ccc1ba8dc6eb6f2a
microwave-tactical <125757557+microwave-tactical@users.noreply.github.com>
2023-02-18 20:47:22 -0500

    Add files via upload

---
+++ MakeUofT-2023-Robotic-Arm/MakeUofT-2023-Robotic-Arm/movments.h
@@ -0,0 +1,55 @@
+
+//entercoirdnets for movments 
+
+
+void moveExample(){
+
+  //base roation
+//  servo2CW(60, 150);
+  //servo2CCW(150, 60);
+
+
+  //delay(200); //delay is needed to make it move in sequrence
+    
+   //arm extension servo3
+   // low 120 if facing switch
+   // low 105 anywhere else
+   // high 70
+
+  //servo3CW(85, 120);
+ // servo3CCW(85, 120);
+
+  //delay(200);
+
+   
+  //base extension servo4
+  // low 180 high 20
+  
+  servo4CW(20, 180);
+  servo4CCW(180, 20);
+
+  delay(200);
+
+  
+    }
+
+
+  /*
+  servo2CW(0, 180);
+  delay(200);
+  servo2CCW(180, 0);
+  delay(200);
+
+   servo3CW(0, 180);
+  delay(200);
+  servo3CCW(180, 0);
+  delay(200);
+
+ servo4CW(0, 180);
+  delay(200);
+  servo4CCW(180, 0);
+  delay(200);
+
+
+ 
+   */
